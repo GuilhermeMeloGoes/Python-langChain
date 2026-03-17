5849235f5cd11c2a115ec87fe3bab9d8a65401af
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-03-17 18:54:48 -0300

    feat: adicionando um modelo de prompt e alterando o nome da variável da chave api

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain import PromptTemplate
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key_google = os.getenv("GOOGLE_API_KEY")
 
 numero_dias = 7
 numero_criancas = 2
-atividade = "musica"
+atividade = "praia"
 
-prompt = f"Crie um roteiro de viagens de {numero_dias} dias, para uma familia com {numero_criancas} crianças, que gosta de {atividade}"
+modelo_prompt = PromptTemplate(
+    template = '''
+        Crie um roteiro de viagem de {dias} dias,
+        para uma família com {numero_criancas} crianças,
+        que gostam de {atividade}.
+    '''
+)
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+prompt = modelo_prompt.format(
+    dias = numero_dias,
+    numero_criancas = numero_criancas,
+    atividade = atividade  
+)
+
+print("Prompt: \n", prompt)
+
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-flash",
+    temperature = 0.5,
+    api_key = api_key_google
+)
 
 mensagens = [
     SystemMessage(content="Você é um assistente de viagens especializado em criar roteiro de viagens personalizados."),
