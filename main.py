4a8cbd1eeb49dd94d1d9456cba009810b0f81da9
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-03-17 19:21:00 -0300

    feat: adicionando a formatação do tipo Json, definindo um modelo de resposta com base em um interesse.

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage, HumanMessage
-from langchain import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from pydantic import Field, BaseModel
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
+
+
+set_debug(True)
 
 load_dotenv()
 api_key_google = os.getenv("GOOGLE_API_KEY")
 
-numero_dias = 7
-numero_criancas = 2
-atividade = "praia"
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo: str = Field("O motivo pelo qual é interessante visitar essa cidade")
 
-modelo_prompt = PromptTemplate(
-    template = '''
-        Crie um roteiro de viagem de {dias} dias,
-        para uma família com {numero_criancas} crianças,
-        que gostam de {atividade}.
-    '''
+parser = JsonOutputParser(pydantic_object=Destino)
+
+prompt_cidade = PromptTemplate(
+    template = """
+        Sugira uma cidade dado o meu interesse por {interesse}.
+        {formato_de_saida}
+    """,
+    input_variables = ["interesse"],
+    partial_variables = {"formato_de_saida": parser.get_format_instructions()}
 )
-
-prompt = modelo_prompt.format(
-    dias = numero_dias,
-    numero_criancas = numero_criancas,
-    atividade = atividade  
-)
-
-print("Prompt: \n", prompt)
 
 llm = ChatGoogleGenerativeAI(
     model = "gemini-2.5-flash",
@@ -33,11 +34,12 @@ llm = ChatGoogleGenerativeAI(
     api_key = api_key_google
 )
 
-mensagens = [
-    SystemMessage(content="Você é um assistente de viagens especializado em criar roteiro de viagens personalizados."),
-    HumanMessage(content=prompt)
-]
+cadeia = prompt_cidade | llm | parser
 
-resposta = llm.invoke(mensagens)
+resposta = cadeia.invoke(
+    {
+        "interesse": "praias"
+    }
+)
 
-print(resposta.content)
+print(resposta)
